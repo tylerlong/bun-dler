015dc0897311ed0ced287d4500a7738d7d957636
Tyler Liu <tyler4long@gmail.com>
2024-12-08 10:11:59 -0800

    Watch mode

--- src/index.ts
+++ src/index.ts
@@ -1,63 +1,86 @@
 #!/usr/bin/env bun
-import { existsSync, mkdirSync, copyFileSync, writeFileSync } from "fs";
+import { existsSync, mkdirSync, copyFileSync, writeFileSync, watch } from "fs";
 import { resolve, join } from "path";
 import { compile as sassCompile } from "sass";
 
-// normalize config
-const config = {
-  outDir: "./public",
-  jsEntries: ["./src/index.ts"],
-  cssEntries: ["./src/index.css"],
-  copyFiles: ["./src/index.html"],
-};
-const configPath = resolve(process.cwd(), "packle.config.json");
-const userConfig = existsSync(configPath) ? require(configPath) : undefined;
-if (userConfig) {
-  for (const key of Object.keys(userConfig)) {
-    if (config[key]) {
-      config[key] = userConfig[key];
+const bundle = () => {
+  // normalize config
+  const config = {
+    outDir: "./public",
+    jsEntries: ["./src/index.ts"],
+    cssEntries: ["./src/index.css"],
+    copyFiles: ["./src/index.html"],
+  };
+  const configPath = resolve(process.cwd(), "packle.config.json");
+  const userConfig = existsSync(configPath) ? require(configPath) : undefined;
+  if (userConfig) {
+    for (const key of Object.keys(userConfig)) {
+      if (config[key]) {
+        config[key] = userConfig[key];
+      }
     }
   }
-}
-// Update path to absolute
-for (const key of Object.keys(config)) {
-  if (Array.isArray(config[key])) {
-    config[key] = config[key].map((p: string) => resolve(process.cwd(), p));
-  } else {
-    config[key] = resolve(process.cwd(), config[key]);
+  // Update path to absolute
+  for (const key of Object.keys(config)) {
+    if (Array.isArray(config[key])) {
+      config[key] = config[key].map((p: string) => resolve(process.cwd(), p));
+    } else {
+      config[key] = resolve(process.cwd(), config[key]);
+    }
   }
-}
 
-// create outDir
-if (!existsSync(config.outDir)) {
-  mkdirSync(config.outDir, { recursive: true });
-}
-
-// copy files
-for (const file of config.copyFiles) {
-  if (!existsSync(file)) {
-    continue;
+  // create outDir
+  if (!existsSync(config.outDir)) {
+    mkdirSync(config.outDir, { recursive: true });
   }
-  const target = join(config.outDir, file.split("/src/")[1]);
-  copyFileSync(file, target);
-}
 
-// bundle js
-Bun.build({
-  entrypoints: config.jsEntries.filter((p) => existsSync(p)),
-  outdir: config.outDir,
-  target: "browser",
-  naming: {
-    asset: "[dir]/[name].[ext]",
-  },
-});
-
-// bundle css
-for (const cssFile of config.cssEntries) {
-  if (!existsSync(cssFile)) {
-    continue;
+  // copy files
+  for (const file of config.copyFiles) {
+    if (!existsSync(file)) {
+      continue;
+    }
+    const target = join(config.outDir, file.split("/src/")[1]);
+    copyFileSync(file, target);
   }
-  const result = sassCompile(cssFile);
-  const target = join(config.outDir, cssFile.split("/src/")[1]);
-  writeFileSync(target, result.css);
+
+  // bundle js
+  Bun.build({
+    entrypoints: config.jsEntries.filter((p) => existsSync(p)),
+    outdir: config.outDir,
+    target: "browser",
+    naming: {
+      asset: "[dir]/[name].[ext]",
+    },
+  });
+
+  // bundle css
+  for (const cssFile of config.cssEntries) {
+    if (!existsSync(cssFile)) {
+      continue;
+    }
+    const result = sassCompile(cssFile);
+    const target = join(config.outDir, cssFile.split("/src/")[1]);
+    writeFileSync(target, result.css);
+  }
+};
+
+bundle();
+
+const debounce = (func) => {
+  let timeout: ReturnType<typeof setTimeout> | null = null;
+  return (...args: Parameters<typeof func>) => {
+    if (timeout) {
+      clearTimeout(timeout);
+    }
+    timeout = setTimeout(() => {
+      func(...args);
+    }, 100);
+  };
+};
+const debouncedBundle = debounce(bundle);
+const inputs = new Set(process.argv);
+if (inputs.has("--watch") || inputs.has("-w")) {
+  watch(resolve(process.cwd(), "packle.config.json"), () => debouncedBundle());
+  watch(resolve(process.cwd(), "package.json"), () => debouncedBundle());
+  watch(resolve(process.cwd(), "src"), () => debouncedBundle());
 }
